ae9721b59ce538ee8443519d258ca3d5d041cb28
Miguel Guerino <miguel.guerino2011@gmail.com>
2025-11-06 21:04:13 -0300

    Update Pimv2.py

--- Pimv2.py
+++ Pimv2.py
@@ -14,9 +14,9 @@ def ler_txt(CAMINHO_ARQUIVO):
   with open(CAMINHO_ARQUIVO,'r') as file:
     conteudo = file.read()
 
-def escrever_txt(CAMINHO_ARQUIVO,aluno, media):
+def escrever_txt(CAMINHO_ARQUIVO,aluno, nota1, nota2):
   with open(CAMINHO_ARQUIVO,'a') as file:
-    file.write(f'{aluno}\n{media}\n')
+    file.write(f'{aluno}\n{nota1}\n{nota2}\n')
   
 def menu_case(numero):
   match numero:
@@ -50,10 +50,10 @@ def cadastro(CAMINHO_ARQUIVO):
   cadastro_aluno = (f'{nome_aluno}\n{ra_aluno}\n{turma_aluno}')
   notas_aluno = float(input('Qual a np1 do aluno: \n'))
   notas_aluno2 = float(input('Qual a np2 do aluno: \n'))
-  media_final = media(notas_aluno,notas_aluno2)
-  escrever_txt(CAMINHO_ARQUIVO,cadastro_aluno,media_final)
+
+  escrever_txt(CAMINHO_ARQUIVO,cadastro_aluno,notas_aluno,notas_aluno2)
   print(f'O cadastro de {nome_aluno} foi feito: \n')
-  print(f'{nome_aluno} {ra_aluno} {turma_aluno} {media_final}')
+  print(f'{nome_aluno} {ra_aluno} {turma_aluno} {notas_aluno} {notas_aluno2}')
 
 while True:
   print('-=-=-=-='*5)
